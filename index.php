cf2834e7c2a3eb61ad22090f2f37a1b0f34bde89
ckolenchenko <ckolenchenko@yukon.cv.ua>
2024-03-03 14:01:48 +0200

    Add jquery

--- index.php
+++ index.php
@@ -2,69 +2,60 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-
     <title>Nawata</title>
-
     <link rel="stylesheet" href="public/css/main.css" type="text/css"/>
-
-    <!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
-    <!--    <script type="text/javascript" src="public/js/main.js"></script>-->
-</head>
-
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <body>
-<div class="site-title">Our Photos</div>
+<div class="site-title">Our Photos&nbsp;&nbsp;&nbsp;<button type="button" id="add_image_btn">
+        Додати
+    </button>
+</div>
 
 <?php
 require_once 'pages/photo.php';
+
+$json_val = json_encode([
+    'posts' => 'posts',
+    'int' => 15,
+    'dbl' => 58.525,
+]);
+
+file_put_contents('images.json', json_encode($json_val));
+
+
 ?>
 
-<!--<table class="main-tbl">-->
-<!--    <tr>-->
-<!--        <td class="control-td"><button class="next-btn" id="next_facet_btn" onclick="getNextFaucet();">Next</button></td>-->
-<!--        <td id="id_td" class="db-id-td hided" >id:&nbsp;undefined</td>-->
-
-<!--        <td id="act_after_td" class="input-control-td hided">Show after-->
-<!--            <input type="text" id="cduraion" name="cduraion" class="time-inp" /> sec.-->
-<!--            <input type="hidden" id="oduraion" name="oduraion" />-->
-<!--        </td>-->
-
-
-<!--        <td id="priority_td" class="input-control-td">Priority&nbsp;-->
-<!--            <input type="text" id="priority" name="priority" class="time-inp" />-->
-<!--        </td>-->
-
-<!--        <td class="num-info-td">-->
-<!--            New test index file <span id="n_all_span"></span> / <span id="n_active_span"></span>-->
-<!--            <span id="last_payment" class="last-payment"></span>-->
-<!--        </td>-->
-
-<!---->
-<!--        <td>-->
-<!--            <div class="site-title">Site to test App links in Flutter project</div>-->
-<!--        </td>-->
-
-
-<!--        <td id="refresh_td" class="control-td hided"><button id="refresh_btn" onclick="refresh();">Refresh</button></td>-->
-<!--        <td id="disable_td" class="control-td hided"><button id="disable_btn" onclick="disableFaucet();">Disable</button></td>-->
-<!--        <td class="control-td"><button id="enable_all_btn" onclick="enableAll();">Enable&nbsp;All</button></td>-->
-<!--    </tr>-->
-
-<!--    <tr>-->
-<!--        <td colspan="8"class="frame-td">-->
-<!--            <iframe id="main_fraim" src="startf.php" class="main-fraim"></iframe>-->
-<!--        </td>-->
-<!--    </tr>-->
-
-<!--</table>-->
 
 </body>
 
+
 <script type="text/javascript">
 
-    // $(document).ready(function(){
-    //     getNextFaucet();
-    // });
+    let ss = 0;
 
+
+    $(document).ready(function () {
+
+        $('#add_image_btn').on('click', function () {
+            console.log('SS value is ' + ss)
+            ss++;
+
+            // var order = {
+            //     name: $('#name').val(),
+            //     grade: $('#grade').val(),
+            // }
+            // $.ajax({
+            //     type: 'POST',
+            //     url: 'json.php',
+            //     data: order,
+            //     success: function (newStudent) {
+            //         console.log("Success");
+            //         $("#students").append(setStudents($('#name').val(), $('#grade').val()));
+            //
+            //     }
+            // })
+        });
+    });
 </script>
 
 </html>
