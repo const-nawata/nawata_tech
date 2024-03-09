c326d6471cf3fa97fa1410083a0ce3a1396626c7
ckolenchenko <ckolenchenko@yukon.cv.ua>
2024-03-09 12:50:02 +0200

    URL parsing.

--- index.php
+++ index.php
@@ -6,28 +6,38 @@
     <link rel="stylesheet" href="public/css/main.css" type="text/css"/>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <body>
-<div class="site-title">Our Photos&nbsp;&nbsp;&nbsp;
-
-
-</div>
+<div class="site-title">Our Photos</div>
 
 <div class="image-host">
     <label for="host">HOST</label>
-    <input type="text" id="host" name="host" size="50">
+    <input type="text" id="host" name="host" size="90">
     <button type="button" id="add_image_btn">Додати</button>
 </div>
-
 
 <?php
 require_once 'pages/photo.php';
 
-$fl_cont = file_get_contents('images.json');
-$data_s = json_decode($fl_cont);
-$data_s2 = json_decode($data_s, true);
+$file_content = file_get_contents('images.json');
+$images_data = json_decode($file_content);
+$images_data = json_decode($images_data, true);
 
-print_r('File: ' . $fl_cont);
-print_r('<br>');
-print_r('JSON: ' . $data_s2['host']);
+print_r("<br><br>File: {$file_content}<br><br>");
+
+$url_data = parse_url($images_data['host']);
+
+print_r('SCHEME: ' . $url_data['scheme'] . '<br>');
+print_r('HOST: ' . $url_data['host'] . '<br>');
+print_r('PATH: ' . $url_data['path'] . '<br>');
+print_r('QUERY: ' . $url_data['query'] . '<br><br>');
+
+parse_str($url_data['query'], $query);
+print_r('WIDTH: ' . $query['width'] . '<br>HEIGHT: ' . $query['height'] . '<br><br>');
+
+$url_main =
+    $url_data['scheme'] . '://' . $url_data['host'] . $url_data['path'] . '?resid=' . urlencode($query['resid']) . '&authkey=' . urlencode($query['authkey']);
+
+print_r('URL: <br>' . $images_data['host'] . '<br>');
+print_r($url_main);
 ?>
 
 
