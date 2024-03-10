cecfdcac2aca8dd9075d1f2f8f54d67e2bda6093
ckolenchenko <ckolenchenko@yukon.cv.ua>
2024-03-10 10:34:45 +0200

    Empty host validating

--- save_image.php
+++ save_image.php
@@ -3,40 +3,36 @@ header('Content-Type: application/json; charset=utf-8');
 
 $file_content = file_get_contents('data.json');
 $data = json_decode($file_content, true);
-$url_data = parse_url($_GET['host']);
-
-$data = json_decode($file_content, true);
-$images_data = $data['images'];
-parse_str($url_data['query'], $query);
-$count = count($images_data);
-
-$resid = urlencode($query['resid']);
 
 $result = true;
 
-for ($i = 0; $i < $count; $i++) {
-    if ($images_data[$i]['resid'] == $resid) {
-        $result = false;
-        break;
+if ($_GET['host'] != '') {
+    $url_data = parse_url($_GET['host']);
+    $data = json_decode($file_content, true);
+    $images_data = $data['images'];
+    parse_str($url_data['query'], $query);
+    $count = count($images_data);
+    $resid = urlencode($query['resid']);
+
+    for ($i = 0; $i < $count; $i++) {
+        if ($images_data[$i]['resid'] == $resid) {
+            $result = false;
+            break;
+        }
+    }
+
+    if ($result) {
+        $data['images'][] = [
+            'scheme' => $url_data['scheme'],
+            'host' => $url_data['host'],
+            'path' => $url_data['path'],
+            'resid' => $resid,
+            'authkey' => urlencode($query['authkey']),
+            'width' => $query['width'],
+            'height' => $query['height'],
+        ];
+
+        file_put_contents('data.json', json_encode($data));
     }
 }
-
-if ($result) {
-    $data['images'][] = [
-        'scheme' => $url_data['scheme'],
-        'host' => $url_data['host'],
-        'path' => $url_data['path'],
-        'resid' => $resid,
-        'authkey' => urlencode($query['authkey']),
-        'width' => $query['width'],
-        'height' => $query['height'],
-    ];
-
-    file_put_contents('data.json', json_encode($data));
-}
-
-
 echo json_encode(['result' => $result]);
-
-
-//wp_send_json_success( 'Bye' );
