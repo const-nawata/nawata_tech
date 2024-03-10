7c610d401b07ca4847501bdb61b6ca1c8182e512
ckolenchenko <ckolenchenko@yukon.cv.ua>
2024-03-10 10:49:13 +0200

    Admin pass check

--- index.php
+++ index.php
@@ -8,15 +8,25 @@
 <body>
 <div class="site-title">Наші фотки</div>
 
-<div class="image-host">
-    <label for="host">HOST</label>
-    <input type="text" id="host" name="host" size="90" value="">
-    <button type="button" id="add_image_btn">Додати</button>
-</div>
-
-<div class="photo-exists" id="div_err">&nbsp;</div>
 
 <?php
+if (array_key_exists('pass', $_GET) && $_GET['pass'] == '0043') {
+    ?>
+    <div class="image-host">
+        <label for="host">HOST</label>
+        <input type="text" id="host" name="host" size="90" value="">
+        <button type="button" id="add_image_btn">Додати</button>
+    </div>
+
+    <div class="photo-exists" id="div_err">&nbsp;</div>
+
+    <?php
+} else {
+    ?>
+    <div class="image-host"></div>
+    <?php
+}
+
 if (!file_exists('data.json')) {
     $images_data = [
         'pass' => '0043',
@@ -26,41 +36,7 @@ if (!file_exists('data.json')) {
 }
 
 require_once 'pages/photo.php';
-
-//$stack = ["orange", "banana"];
-//$stack[] = "orange";
-//$stack[] = "raspberry";
-//print_r($stack);
-//
-//$file_content = file_get_contents('data.json');
-//$images_data = json_decode($file_content, true);
-//
-//print_r("<br><br>File: {$file_content}<br><br>");
-//
-//$count = count($images_data);
-//
-//$url_data = parse_url($images_data[0]['host']);
-//
-//print_r('Count: ' . $count . '<br>');
-//print_r('SCHEME: ' . $url_data['scheme'] . '<br>');
-//print_r('HOST: ' . $url_data['host'] . '<br>');
-//print_r('PATH: ' . $url_data['path'] . '<br>');
-//print_r('QUERY: ' . $url_data['query'] . '<br><br>');
-//
-//parse_str($url_data['query'], $query);
-//
-//
-//print_r('WIDTH: ' . $images_data[0]['width'] . '<br>HEIGHT: ' . $images_data[0]['height'] . '<br><br>');
-//
-//$url_main =
-//    $url_data['scheme'] . '://' . $url_data['host'] . $url_data['path'] . '?resid=' . urlencode($query['resid']) . '&authkey=' . urlencode($query['authkey']);
-//
-//print_r('URL: <br>' . $images_data[0]['host'] . '<br>');
-//print_r($url_main);
-
 ?>
-
-
 </body>
 
 <script type="text/javascript">
