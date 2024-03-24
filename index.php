ae36cc3ad778a44150374e9ca3bd18b77b4d7b51
ckolenchenko <ckolenchenko@yukon.cv.ua>
2024-03-24 10:47:50 +0200

    Re-factor

--- index.php
+++ index.php
@@ -100,11 +100,11 @@ require_once 'pages/photo.php';
         return document.getElementById(id);
     }
 
-    let show = function (id) {
-        $(id).style.display = 'block';
+    let show = function () {
+        $("expandedImg").style.display = 'block';
     }
-    let hide = function (id) {
-        $(id).style.display = 'none';
+    let hide = function () {
+        $("expandedImg").style.display = 'none';
     }
 
 
--- pages/photo.php
+++ pages/photo.php
@@ -1,4 +1,4 @@
-<div style="text-align: center;" onclick="show('expandedImg')">
+<div style="text-align: center;" onclick="show()">
     <?php
     $file_content = file_get_contents('data.json');
     $data = json_decode($file_content, true);
