8b4c859d9b00e77a8a58759c83bbdb42fb3f4780
ckolenchenko <ckolenchenko@yukon.cv.ua>
2024-03-28 17:06:53 +0200

    Set Del button on every image

--- pages/photo.php
+++ pages/photo.php
@@ -1,38 +1,25 @@
 <div class="ourImages">
+
+
     <?php
     $file_content = file_get_contents('data.json');
     $data = json_decode($file_content, true);
     $images_data = $data['images'];
     $count = count($images_data);
 
+    // &#x2421;
+
     for ($i = 0; $i < $count; $i++) {
         $url =
             $images_data[$i]['scheme'] . '://' . $images_data[$i]['host'] . $images_data[$i]['path'] . '?resid=' .
             $images_data[$i]['resid'] . '&authkey=' . $images_data[$i]['authkey'] . '&width=300';
         ?>
-        <!--        <img src="--><?php //= $url ?><!--" alt="image00000" width="300" onclick="showExpanded();" class="imgItem">-->
-        <img src="<?= $url ?>" alt="image00000" width="300" class="imgItem">
+        <div class="container">
+            <div class="tag">&#9747;</div>
+            <img src="<?= $url ?>" alt="image00000" width="300" class="imgItem">
+        </div>
         <?php
     }
     ?>
 </div>
-
-<?php
-/*
-?>
-
-<div style="text-align: center;">
-    <img src="https://onedrive.live.com/embed?resid=E56760D01EE8AF8D%21407712&authkey=%21ADJwf_qNiN5H5_c&width=660"
-         alt="image00000" width="300">
-
-    <img src="https://onedrive.live.com/embed?resid=E56760D01EE8AF8D%21407713&authkey=%21AP6OiZxl8HhC6oE&width=660"
-         alt="image00001" width="300">
-
-    <img src="https://onedrive.live.com/embed?resid=E56760D01EE8AF8D%21407714&authkey=%21AJjf-FrqT5iTFfc&width=660"
-         alt="Нещастя" width="300"">
-</div>
-
-<?php
-*/
-?>
 
